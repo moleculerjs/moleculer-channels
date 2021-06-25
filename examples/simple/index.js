72b745ff9bf629b31dcce26289675947c598621b
Icebob <mereg.norbert@gmail.com>
2021-06-25 22:49:33 +0200

    detailed example

--- examples/simple/index.js
+++ examples/simple/index.js
@@ -8,6 +8,25 @@ const broker = new ServiceBroker({
 	middlewares: [ChannelsMiddleware()]
 });
 
+broker.createService({
+	name: "posts",
+	version: 1,
+	channels: {
+		async "my.first.topic"(msg) {
+			this.logger.info("Channel One msg received", msg);
+		},
+
+		"my.second.topic": {
+			group: "other",
+			// maxInFlight: 1,
+			// adapter: "kafka://kafka:9092",
+			async handler(msg) {
+				this.logger.info("Channel Two msg received", msg);
+			}
+		}
+	}
+});
+
 broker.start().then(async () => {
 	broker.repl();
 });
--- src/index.js
+++ src/index.js
@@ -7,51 +7,34 @@
 "use strict";
 
 const _ = require("lodash");
-const pkg = require("../package.json");
 
-module.exports = function DatabaseMixin(mixinOpts) {
-	mixinOpts = _.defaultsDeep(mixinOpts, {});
+module.exports = function ChannelsMiddleware(mwOpts) {
+	mwOpts = _.defaultsDeep(mwOpts, {});
+	let logger;
 
-	const schema = {
-		// Must overwrite it
-		name: "",
-
-		/**
-		 * Metadata
-		 */
-		// Service's metadata
-		metadata: {
-			$category: "messages",
-			$description: "Reliable messages for Moleculer services",
-			$official: true,
-			$package: {
-				name: pkg.name,
-				version: pkg.version,
-				repo: pkg.repository ? pkg.repository.url : null
-			}
-		},
-
-		/**
-		 * Default settings
-		 */
-		settings: {},
-
-		/**
-		 * Actions
-		 */
-		actions: {},
-
-		/**
-		 * Methods
-		 */
-		methods: {},
+	return {
+		name: "Channels",
 
 		/**
 		 * Create lifecycle hook of service
 		 */
 		created(broker) {
+			logger = broker.getLogger("Channels");
 			// TODO: Populate broker with new methods
-			broker.logger.info("Hello");
+		},
+
+		/**
+		 * Created lifecycle hook of service
+		 */
+		async serviceCreated(svc) {
+			if (_.isPlainObject(svc.schema.channels)) {
+				logger.info(
+					`Processing channels of '${svc.fullName}' service...`,
+					svc.schema.channels
+				);
+				// TODO: Process `channels` in the schema
+				// TODO: collect custom adapters
+			}
 		},
 
 		/**
@@ -66,18 +49,6 @@ module.exports = function DatabaseMixin(mixinOpts) {
 		 */
 		async stopped() {
 			// TODO: Disconnect adapters
-		},
-
-		/**
-		 * It is called when the Service schema mixins are merged. At this
-		 * point, we read channel schemas and make the subscriptions.
-		 *
-		 * @param {Object} schema
-		 */
-		merged(schema) {
-			// TODO: Process `channels` in the schema
 		}
 	};
-
-	return schema;
 };
