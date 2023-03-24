2c8c0d1b4dda8749ec5779c4444911c3f639ab86
Ujwal Setlur <ujwal.setlur@gmail.com>
2023-03-24 01:09:30 -0700

    added a default name for the dead letter exchange

--- src/adapters/amqp.js
+++ src/adapters/amqp.js
@@ -303,6 +303,8 @@ class AmqpAdapter extends BaseAdapter {
 				chan.deadLettering.queueName = this.addPrefixTopic(chan.deadLettering.queueName);
 				chan.deadLettering.exchangeName = this.addPrefixTopic(
 					chan.deadLettering.exchangeName
+						? chan.deadLettering.exchangeName
+						: "DEAD_LETTER"
 				);
 
 				this.logger.debug(`Asserting exchange ${chan.deadLettering.exchangeName}`);
